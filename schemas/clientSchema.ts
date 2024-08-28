f6ac4def1101e5f4fe3bba00d76e66f832f2bbf6
Shorno <roy15-6141@s.diu.edu.bd>
2024-08-28 07:03:22 +0600

    schema changes

--- schemas/clientSchema.ts
+++ schemas/clientSchema.ts
@@ -8,27 +8,38 @@ export const clientSchema = z.object({
     address: z.string()
         .min(1, {message: "Please enter the client's address."})
         .min(3, {message: "Address should be at least 3 characters long."}),
-    serialNumber: z.string()
-        .min(1, {message: "Client's serial is required."})
+    serialNumber: z.number().int().min(1, {message: "Serial number is required."}),
+    loanGuarantorPhone: z.string()
+        .min(1, {message: "Guarantor's phone number is required."})
+        .max(11, {message: "Guarantor's phone number is invalid."}).optional(),
+    loanGuarantorName: z.string()
+        .min(1, {message: "Guarantor's name is required."}).optional(),
+    loanGuarantorAddress: z.string()
+        .min(1, {message: "Guarantor's address is required."}).optional()
 });
 
 export const loanSchema = z.object({
-    amount: z.string()
+    amount: z.number().int()
         .min(1, {message: "Loan amount is required."}),
-    interestRate: z.string()
+    interestRate: z.number().int()
         .min(1, {message: "Interest rete is not added"}),
-    totalPayable: z.string().optional(),
-    paidAmount: z.string()
+    totalPayable: z.number().int().optional(),
+    paidAmount: z.number().int()
         .min(1, {message: "Paid amount is required."}),
-    remainingAmount: z.string().optional(),
+    remainingAmount: z.number().int().optional(),
+    totalSavings: z.number().int().optional(),
+    startDate: z.coerce.date()
+        .min(new Date("2000-01-01"), {message: "Date must be after year 2000"})
+        .max(new Date(), {message: "Date cannot be in the future"}),
 });
 
 export const paymentSchema = z.object({
-    amount: z.string()
-        .min(0.01, {message: "Payment amount is required."}),
+    amount: z.number().int()
+        .min(1, {message: "Payment amount is required."}),
     date: z.coerce.date()
         .min(new Date("2000-01-01"), {message: "Date must be after year 2000"})
         .max(new Date(), {message: "Date cannot be in the future"}),
+    savings: z.number().int().min(1, {message: "Savings amount is required."}),
 });
 
 
